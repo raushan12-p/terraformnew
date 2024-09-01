24713a3326589846a0481f4b7be238f462b17c40
raushan12-p <raushan1054@gmail.com>
2024-09-01 16:15:04 +0530

    update local

--- day-5/main.tf
+++ day-5/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "test" {
     ami = "ami-0ae8f15ae66fe8cda"
     instance_type = "t2.micro"
-    availability_zone = "us-east-1a"
+    availability_zone = "us-east-1b"
     key_name = "rau-key"
     
   tags = {
